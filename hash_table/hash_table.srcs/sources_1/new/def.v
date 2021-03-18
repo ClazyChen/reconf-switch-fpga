853963841c70edc53992fe9130181b9392dd8473
jijinfanhua <1446353452@qq.com>
2021-03-18 11:50:08 +0800

    add hash table

---
+++ hash_table/hash_table.srcs/sources_1/new/def.v
@@ -0,0 +1,11 @@
+
+`define DATA_WIDTH  32
+`define ADDR_WIDTH  10
+
+`define KEY_LENGTH  22
+`define VALUE_LENGTH    10
+
+`define HASH_INSERT 2'b00
+`define HASH_SEARCH 2'b01
+`define HASH_DELETE 2'b10
+`define HASH_MODIFY 2'b11
